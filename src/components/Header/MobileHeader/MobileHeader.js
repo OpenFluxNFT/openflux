8e840660a367f15d019f6892842db593a336123b
LorenaL579 <lorena.lici579@gmail.com>
2023-12-13 14:57:05 +0100

    ui fixes and optimizations

--- src/components/Header/MobileHeader/MobileHeader.js
+++ src/components/Header/MobileHeader/MobileHeader.js
@@ -1,79 +1,135 @@
 import React from "react";
 import "./_mobileheader.scss";
-import '../_header.scss'
+import "../_header.scss";
 import dypiusLogo from "../assets/dypiusLogo.svg";
 import { useState } from "react";
 import { useEffect } from "react";
-import mobileNavArrow from './mobileNavArrow.svg'
+import mobileNavArrow from "./mobileNavArrow.svg";
 import { shortAddress } from "../../../hooks/shortAddress";
-import cartIcon from '../../Header/assets/cartIcon.svg'
-import walletIcon from '../../Header/assets/walletIcon.svg'
-import userIcon from '../../Header/assets/userIcon.svg'
+import walletIcon from "../../Header/assets/walletIcon.svg";
+import userIcon from "../../Header/assets/userIcon.svg";
+import { handleSwitchNetworkhook } from "../../../hooks/switchNetwork";
+import conflux from "../assets/conflux.svg";
+import { NavLink } from "react-router-dom";
 
-const MobileHeader = ({handleSignup, coinbase}) => {
+const MobileHeader = ({
+  handleSignup,
+  coinbase,
+  isConnected,
+  chainId,
+  handleSwitchNetwork,
+}) => {
+  const [menu, setMenu] = useState(false);
 
-    const [menu, setMenu] = useState(false)
+  const html = document.querySelector("html");
 
-    const html = document.querySelector("html");
-  
-  
-    useEffect(() => {
-      if (menu) {
-        html.classList.add('hidescroll')
-      } else {
-        // Enable scroll
-        html.classList.remove('hidescroll')
+  const handleConfluxPool = async () => {
+    if (window.ethereum) {
+      if (!window.gatewallet) {
+        await handleSwitchNetworkhook("0x406")
+          .then(() => {
+            handleSwitchNetwork(1030);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
       }
-  
-  
-  
-    }, [menu]);
-  
+    } else {
+      window.alertify.error("No web3 detected. Please install Metamask!");
+    }
+  };
+
+  useEffect(() => {
+    if (menu) {
+      html.classList.add("hidescroll");
+    } else {
+      // Enable scroll
+      html.classList.remove("hidescroll");
+    }
+  }, [menu]);
+
   return (
     <>
-    <div className="container-fluid py-4 mobile-header-wrapper header-wrapper " style={{height: "85px", pointerEvents: "auto", zIndex: 5}}>
-      <div className="container-lg">
-        <div className="row align-items-center justify-content-center">
-          <div className="d-flex align-items-center justify-content-between">
-            <img src={dypiusLogo} width={35} height={35} alt="" />
-            <>
-              <input id="menu-toggle" type="checkbox" onChange={() => {setMenu(!menu)}} />
-              <label class="menu-button-container" for="menu-toggle">
-                <div class="menu-button"></div>
-              </label>
-            </>
+      <div
+        className="container-fluid py-4 mobile-header-wrapper header-wrapper "
+        style={{ height: "85px", pointerEvents: "auto", zIndex: 5 }}
+      >
+        <div className="container-lg">
+          <div className="row align-items-center justify-content-center">
+            <div className="d-flex align-items-center justify-content-between">
+              <img src={dypiusLogo} width={35} height={35} alt="" />
+              <>
+                <input
+                  id="menu-toggle"
+                  type="checkbox"
+                  onChange={() => {
+                    setMenu(!menu);
+                  }}
+                />
+                <label class="menu-button-container" for="menu-toggle">
+                  <div class="menu-button"></div>
+                </label>
+              </>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className={`mobile-menu-container ${menu && "mobile-active"} d-flex flex-column align-items-center justify-content-center gap-3 p-3`}>
+      <div
+        className={`mobile-menu-container ${
+          menu && "mobile-active"
+        } d-flex flex-column align-items-center justify-content-center gap-3 p-3`}
+      >
+        <NavLink
+          className={
+            "mobile-menu-item d-flex align-items-center justify-content-between p-3"
+          }
+          to={"/collections"}
+          onClick={() => {
+            setMenu(false);
+          }}
+        >
+          <h6 className="mb-0 text-white">Collections</h6>
+          <img src={mobileNavArrow} alt="" />
+        </NavLink>
+
         <div className="mobile-menu-item d-flex align-items-center justify-content-between p-3">
-            <h6 className="mb-0 text-white">Collections</h6>
-            <img src={mobileNavArrow} alt="" />
+          <h6 className="mb-0 text-white">Mint</h6>
+          <img src={mobileNavArrow} alt="" />
         </div>
         <div className="mobile-menu-item d-flex align-items-center justify-content-between p-3">
-            <h6 className="mb-0 text-white">Mint</h6>
-            <img src={mobileNavArrow} alt="" />
-        </div>
-        <div className="mobile-menu-item d-flex align-items-center justify-content-between p-3">
-            <h6 className="mb-0 text-white">Support</h6>
-            <img src={mobileNavArrow} alt="" />
+          <h6 className="mb-0 text-white">Support</h6>
+          <img src={mobileNavArrow} alt="" />
         </div>
         <div className="d-flex align-items-center gap-3">
-              <button className="btn blue-btn d-flex align-items-center gap-2" 
-                  onClick={handleSignup}
-              >
-                <img src={walletIcon} alt="" />
-                {!coinbase ? "Connect Wallet" : shortAddress(coinbase)}
-              </button>
-              <button className="btn blue-btn" onClick={() => console.log("hello")}>
-                <img src={userIcon} alt="" />
-              </button>
-              <button className="btn blue-btn">
-                <img src={cartIcon} alt="" />
-              </button>
-            </div>
-    </div>
+          {coinbase && isConnected && chainId === 1030 && (
+            <button className="btn account-btn d-flex align-items-center gap-2">
+              <img src={conflux} alt="" /> Conflux
+            </button>
+          )}
+          {coinbase && isConnected && chainId !== 1030 && (
+            <button className="btn error-btn" onClick={handleConfluxPool}>
+              Unsupported chain
+            </button>
+          )}
+          {!coinbase && !isConnected && (
+            <button
+              className="btn blue-btn d-flex align-items-center gap-2"
+              onClick={handleSignup}
+            >
+              <img src={walletIcon} alt="" />
+              Connect Wallet
+            </button>
+          )}
+          {coinbase && isConnected && (
+            <button className="btn account-btn d-flex align-items-center gap-2">
+              {shortAddress(coinbase)}
+            </button>
+          )}
+          <button className="btn blue-btn" onClick={() => console.log("hello")}>
+            <img src={userIcon} alt="" />
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Home/HomeStats/HomeStats.js
+++ src/components/Home/HomeStats/HomeStats.js
@@ -4,7 +4,7 @@ import "./_homestats.scss";
 const HomeStats = () => {
   return (
     <div className="container-fluid  py-5 homestats-wrapper">
-      <div className="container-lg px-0 d-flex flex-column flex-lg-row align-items-center justify-content-between mt-4">
+      <div className="container-lg px-0 d-flex flex-column flex-lg-row align-items-center justify-content-between mt-4 gap-2 gap-lg-0">
         <div className="hero-stats-item p-5 d-flex flex-column align-items-center justify-content-center gap-2">
           <h6 className="stats-item-amount mb-0">47,964</h6>
           <span className="stats-item-desc mb-0">Total transactions</span>

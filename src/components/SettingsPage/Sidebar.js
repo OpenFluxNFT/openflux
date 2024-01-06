0d516919865c0d23aaf9e9ba1a2b33c7ddbcc8ea
aldialinj <aldialinj0@gmail.com>
2024-01-07 00:06:54 +0100

    started working on other parts of settings

--- src/components/SettingsPage/Sidebar.js
+++ src/components/SettingsPage/Sidebar.js
@@ -6,23 +6,23 @@ import notificationsIcon from './assets/notificationsIcon.svg'
 import securityIcon from './assets/securityIcon.svg'
 
 
-const Sidebar = () => {
+const Sidebar = ({onChangeCategory, category}) => {
   return (
     <div className="col-2">
-      <div className="sidebar-wrapper py-3 d-flex flex-column gap-3">
-        <div className="sidebar-item sidebar-item-active px-3 py-2 d-flex align-items-center gap-2">
+      <div className="sidebar-wrapper py-3 d-flex flex-column gap-3" style={{height: "100%"}}>
+        <div className={`sidebar-item ${category === "profile" && "sidebar-item-active"} px-3 py-2 d-flex align-items-center gap-2`} onClick={() => onChangeCategory("profile")}>
             <img src={profileIcon} alt="" />
             <h6 className="mb-0">Profile</h6>
         </div>
-        <div className="sidebar-item px-3 py-2 d-flex align-items-center gap-2">
+        <div className={`sidebar-item ${category === "notifications" && "sidebar-item-active"} px-3 py-2 d-flex align-items-center gap-2`} onClick={() => onChangeCategory("notifications")}>
             <img src={notificationsIcon} alt="" />
             <h6 className="mb-0">Notifications</h6>
         </div>
-        <div className="sidebar-item px-3 py-2 d-flex align-items-center gap-2">
+        <div className={`sidebar-item ${category === "support" && "sidebar-item-active"} px-3 py-2 d-flex align-items-center gap-2`} onClick={() => onChangeCategory("support")}>
             <img src={securityIcon} alt="" />
             <h6 className="mb-0">Support</h6>
         </div>
-        <div className="sidebar-item px-3 py-2 d-flex align-items-center gap-2">
+        <div className={`sidebar-item ${category === "collection" && "sidebar-item-active"} px-3 py-2 d-flex align-items-center gap-2`} onClick={() => onChangeCategory("collection")}>
             <img src={collectionsIcon} alt="" />
             <h6 className="mb-0">Collection</h6>
         </div>
--- src/components/SettingsPage/SupportSettings.js
+++ src/components/SettingsPage/SupportSettings.js
@@ -1,11 +1,44 @@
-import React from 'react'
-import './_settingspage.scss'
-
+import React from "react";
+import "./_settingspage.scss";
 
 const SupportSettings = () => {
   return (
-    <div>SupportSettings</div>
-  )
-}
+    <div className="col-12 col-lg-10">
+      <div className="row">
+        <h6 className="settings-header mb-3">Account Support</h6>
+        <div className="col-12">
+          <div className="d-flex flex-column gap-4">
+            <div className="d-flex flex-column gap-2">
+              <h6 className="input-label mb-0">Subject</h6>
+              <input
+                type="text"
+                placeholder="Enter the topic of the support you need..."
+                className="settings-input w-100"
+              />
+            </div>
+            <div className="d-flex flex-column gap-2">
+              <h6 className="input-label mb-0">Description</h6>
+              <textarea
+                type="text"
+                placeholder="Describe your issue..."
+                className="settings-input w-100"
+                style={{ height: "100%" }}
+                rows={5}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="d-flex align-items-center justify-content-center mt-4">
+          <button
+            className="connect-social-btn px-3 py-1"
+            style={{ fontSize: "16px" }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SupportSettings
+export default SupportSettings;

3bd364441a48591f89f07fdac724f2089513e617
AldiAlinj <aldialinj0@gmail.com>
2024-01-10 15:08:13 +0100

    started working on upload images

--- src/components/SettingsPage/ProfileSettings.js
+++ src/components/SettingsPage/ProfileSettings.js
@@ -1,10 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_settingspage.scss";
 import infoIcon from "./assets/infoIcon.svg";
 import twitterIcon from "./assets/twitterIcon.svg";
 import instagramIcon from "./assets/instagramIcon.svg";
+import editIcon from "./assets/editIcon.svg";
 
 const ProfileSettings = () => {
+  const [profileImage, setProfileImage] = useState();
+  const [bannerImage, setBannerImage] = useState();
+
+  const isImage = async(file) => {
+    const acceptedImageTypes = ["image/png", "image/jpeg", "image/jpg"];
+    return acceptedImageTypes.includes(file.type);
+  };
+
+ 
+
+  const isAspectRatioValidProfile = async (file, targetWidth, targetHeight) => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.onload = () => {
+        resolve(img.width / img.height === targetWidth / targetHeight);
+      };
+      img.src = URL.createObjectURL(file);
+    });
+  };
+
+  const uploadProfileImage =  async (e) => {
+    const maxSizeInBytes = 500 * 1024; // 500KB
+
+    const files = e.target.files;
+
+    if (files.length > 0) {
+      const file = files[0];
+
+      if (
+        file &&
+         await isImage(file) &&
+        //  isSizeValid(file) &&
+        file.size <=  maxSizeInBytes &&
+        await isAspectRatioValidProfile(file, 1, 1)
+      ) {
+        // Set the selected image
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          // Set the selected image as a data URL
+          setProfileImage(reader.result);
+        
+        };
+        reader.readAsDataURL(file);
+      } else {
+        // Display an error message or handle invalid file type, size, or aspect ratio
+        if (!isImage(file)) {
+          alert("Please select a valid image file (PNG, JPG, JPEG)");
+        } else if (file.size >  maxSizeInBytes) {
+          alert("Selected image exceeds the maximum size of 500KB");
+        } else {
+          alert("Selected image must have an aspect ratio of 1:1");
+        }
+      }
+    }
+  };
+  const uploadBannerImage =  async (e) => {
+    const maxSizeInBytes = 500 * 1024; // 500KB
+
+    const files = e.target.files;
+
+    if (files.length > 0) {
+      const file = files[0];
+
+      if (
+        file &&
+         await isImage(file) &&
+        file.size <=  maxSizeInBytes &&
+        await isAspectRatioValidProfile(file, 4, 1)
+      ) {
+        // Set the selected image
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          // Set the selected image as a data URL
+          setBannerImage(reader.result);
+        
+        };
+        reader.readAsDataURL(file);
+      } else {
+        // Display an error message or handle invalid file type, size, or aspect ratio
+        if (!isImage(file)) {
+          alert("Please select a valid image file (PNG, JPG, JPEG)");
+        } else if (file.size >  maxSizeInBytes) {
+          alert("Selected image exceeds the maximum size of 500KB");
+        } else {
+          alert("Selected image must have an aspect ratio of 4:1");
+        }
+      }
+    }
+  };
+
   return (
     <div className="col-12 col-lg-10">
       <div className="row">
@@ -43,22 +136,71 @@ const ProfileSettings = () => {
         <div className="col-12 col-lg-6">
           <div className="d-flex flex-column justify-content-between mt-4 mt-lg-0 h-100">
             <div className="d-flex align-items-center">
-              <div className="col-4">
+              <div className="col-3">
                 <div className="d-flex flex-column gap-2">
                   <div className="d-flex align-items-center gap-2">
                     <h6 className="input-label mb-0">Profile Image</h6>
                     <img src={infoIcon} width={16} height={16} alt="" />
                   </div>
-                  <div className="profile-image-placeholder"></div>
+                  <div className="profile-image-placeholder">
+                    <input
+                      type="file"
+                      accept=".png, .jpg, .jpeg"
+                      onChange={uploadProfileImage}
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        opacity: 0,
+                        cursor: "pointer",
+                      }}
+                    />
+                    {profileImage && (
+                      <img
+                        src={profileImage}
+                        className="profile-image"
+                        alt=""
+                      />
+                    )}
+                    <img src={editIcon} className="edit-image" alt="" />
+                  </div>
                 </div>
               </div>
-              <div className="col-8">
+              <div className="col-9">
                 <div className="d-flex flex-column gap-2">
                   <div className="d-flex align-items-center gap-2">
                     <h6 className="input-label mb-0">Profile Banner</h6>
                     <img src={infoIcon} width={16} height={16} alt="" />
                   </div>
-                  <div className="profile-banner-placeholder"></div>
+                  <div className="profile-banner-placeholder">
+                    {
+
+                    }
+                  <input
+                      type="file"
+                      accept=".png, .jpg, .jpeg"
+                      onChange={uploadBannerImage}
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        opacity: 0,
+                        cursor: "pointer",
+                      }}
+                    />
+                    {bannerImage && (
+                      <img
+                        src={bannerImage}
+                        className="banner-image"
+                        alt=""
+                      />
+                    )}
+                    <img src={editIcon} className="edit-image" alt="" />
+                  </div>
                 </div>
               </div>
             </div>
@@ -109,8 +251,12 @@ const ProfileSettings = () => {
             </div>
           </div>
           <div className="d-flex align-items-center justify-content-center mt-4">
-          <button className="connect-social-btn px-3 py-1" style={{fontSize: "16px"}}>Save</button>
-
+            <button
+              className="connect-social-btn px-3 py-1"
+              style={{ fontSize: "16px" }}
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>

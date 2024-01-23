4eac2f4f4714ca8fa9a4de3f1e69a139daffe14e
AldiAlinj <aldialinj0@gmail.com>
2024-01-23 12:23:53 +0100

    styled make offer popup

--- src/components/MakeOffer/MakeOffer.js
+++ src/components/MakeOffer/MakeOffer.js
@@ -7,8 +7,9 @@ import OutsideClickHandler from "react-outside-click-handler";
 import useWindowSize from "../../hooks/useWindowSize";
 import dropdownIcon from "./assets/dropdownIcon.svg";
 import whiteTag from "./assets/whiteTag.svg";
+import confluxIcon from "./assets/confluxIcon.svg";
 import Web3 from "web3";
-
+import "./makeoffer.scss";
 
 const MakeOffer = ({
   open,
@@ -30,6 +31,7 @@ const MakeOffer = ({
   nftId,
   deletestatus,
   updatestatus,
+  showPopup
 }) => {
   const windowSize = useWindowSize();
   const [filter1, setFilter1] = useState("weth");
@@ -49,8 +51,6 @@ const MakeOffer = ({
 
   const { BigNumber } = window;
 
-
-
   const style = {
     position: "absolute",
     top: "50%",
@@ -68,9 +68,9 @@ const MakeOffer = ({
     height: windowSize.width < 500 ? "480px" : "auto",
   };
 
-
   return (
-    <Modal
+    <OutsideClickHandler onOutsideClick={() => showPopup(false)}>
+      <Modal
       open={open}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
@@ -82,11 +82,11 @@ const MakeOffer = ({
             <img
               src={closeX}
               alt=""
-              className="close-x"
+            
               onClick={() => {
-                onclose();
+                showPopup(false)
               }}
-              style={{ bottom: "17px", right: "-22px", width: "auto" }}
+              style={{ bottom: "17px", right: "-22px", width: "auto", cursor: "pointer" }}
             />
           </div>
           <div className="summarywrapper">
@@ -94,65 +94,61 @@ const MakeOffer = ({
               <div className="d-flex flex-column w-100 flex-xxl-row flex-lg-row  align-items-center gap-2">
                 <img
                   className="p-0 nft-img nftimg2"
-                  src={
-                    'https://mint.dyp.finance/thumbs50/423.png'
-                  }
+                  src={"https://mint.dyp.finance/thumbs50/423.png"}
                   alt=""
                   loading="lazy"
                 />
-                <div className="d-flex flex-column justify-content-between">
-                  <div className="d-flex flex-column align-items-center">
-                    <span className="itemname">
-                    CAWS #1264
+                <div className="d-flex flex-column gap-2">
+                  <div className="d-flex flex-column">
+                    <h6 className="itemname mb-0">CAWS #1264</h6>
+                    <span className="collection-name">
+                      Cats And Watches Society
                     </span>
-                    {/* <span className="itemcollectionName">
-                   {nft.name}
-                  </span> */}
                   </div>
-                  <span className="itemchain">Chain: Conflux</span>
+
+                  <div className="d-flex align-items-center gap-1">
+                    <span className="collection-name">Chain:</span>
+                    <span
+                      className="collection-name"
+                      style={{ color: "white" }}
+                    >
+                      Conflux Network
+                    </span>
+                  </div>
                 </div>
               </div>
-            
-                <div className="d-flex flex-row flex-lg-column flex-xxl-column gap-2 gap-lg-0 gap-xxl-0 align-items-xxl-end align-items-lg-end align-items-center">
-                  <span className="itemname" style={{ whiteSpace: "nowrap" }}>
+
+              <div className="d-flex flex-row flex-lg-column flex-xxl-column gap-2 gap-lg-0 gap-xxl-0 align-items-xxl-end align-items-lg-end align-items-center">
+                <span className="itemname" style={{ whiteSpace: "nowrap" }}>
                   1254.89 CFX
-                  </span>
-                  <span className="itemcollectionName">
-                  ($ 654,874.86)
-                  </span>
-                </div>
-            
+                </span>
+                <span className="itemcollectionName">($ 654,874.86)</span>
+              </div>
             </div>
           </div>
           <div className="summarywrapper">
             <div className="d-flex flex-column align-items-center justify-content-between">
               <div className="d-flex w-100 align-items-center gap-3 justify-content-between">
                 <span className="itemchain">Balance</span>
-                <span className="itemchain">
-                1254.89 CFX
-                </span>
+                <span className="itemvalue">1254.89 CFX</span>
               </div>
               <div className="d-flex w-100 align-items-center gap-3 justify-content-between">
                 <span className="itemchain">Floor price</span>
-                <span className="itemchain">
-                1254.89 CFX
-                </span>
+                <span className="itemvalue">1254.89 CFX</span>
               </div>
               {offerData.length > 0 && (
                 <div className="d-flex  w-100 align-items-center gap-3 justify-content-between">
                   <span className="itemchain">Best offer</span>
-                  <span className="itemchain">
-                  1254.89 CFX
-                  </span>
+                  <span className="itemvalue">1254.89 CFX</span>
                 </div>
               )}
             </div>
           </div>
           <div className="summaryseparator"></div>
           {offerData.length > 0 && (
-            <div className="summaryred">
+            <div className="summaryblue p-2">
               <div className="d-flex align-items-center gap-2 justify-content-between w-100">
-                <span className="itemchain">
+                <span className="myoffer-title">
                   <img src={whiteTag} alt="" /> My offer
                 </span>
                 <div className="d-flex flex-row flex-lg-column flex-xxl-column gap-2 gap-lg-0 gap-xxl-0 align-items-end">
@@ -166,7 +162,6 @@ const MakeOffer = ({
                       : "DYPv1"}
                   </span>
                   <span className="itemcollectionName">
-                    $
                     {getFormattedNumber(
                       offerData[0].offer.payment.priceType === "0"
                         ? ethTokenData * (offerData[0].offer[0] / 1e18)
@@ -181,107 +176,125 @@ const MakeOffer = ({
               </div>
             </div>
           )}
-          <div className="d-flex flex-column flex-xxl-row flex-xl-row flex-lg-row flex-md-row align-items-center gap-3 justify-content-between">
-            <div className="d-flex flex-column flex-xxl-row flex-xl-row flex-lg-row flex-md-row align-items-center gap-2">
+          <div className="row">
+            <div className="col-12 col-lg-6">
               <input
                 type="number"
                 min={0}
                 pattern="^[0-9]*[.,]?[0-9]*$"
                 placeholder="Price"
-                className="px-3 py-2 offerInput"
+                className="px-3 py-2 offerInput categories-dropdown"
                 value={price}
                 onClickCapture={() => {
                   setprice(price != 0 ? price : "");
                 }}
                 onChange={(e) => {
                   setprice(e.target.value === "" ? "" : Number(e.target.value));
-                //   isapprovedMakeOffer(
-                //     Number(e.target.value),
-                //     filter1 === "weth" ? 0 : 1,
-                //     filter1
-                //   );
+                  //   isapprovedMakeOffer(
+                  //     Number(e.target.value),
+                  //     filter1 === "weth" ? 0 : 1,
+                  //     filter1
+                  //   );
                 }}
               />
-              <span className="itemcollectionName">
-                $tbd
-               
-              </span>
             </div>
-            <div className="dropdown" style={{ width: "150px" }}>
-              <button
-                className="btn btn-secondary nft-dropdown w-100
-                 d-flex align-items-center justify-content-between dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <div className="d-flex align-items-center gap-2">
-                  <h6
-                    className="filter-nav-title mb-0"
-                    style={{ textTransform: "uppercase" }}
-                  >
-                    CFX
-                  </h6>
-                </div>
-              </button>
-             
+            <div className="col-2 col-lg-3">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary categories-dropdown p-3 dropdown-toggle  d-flex align-items-center justify-content-center justify-content-lg-between"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ width: windowSize.width > 786 ? "100%" : "34px" }}
+                >
+                  1 day
+                </button>
+                <ul className="dropdown-menu categories-dropdown-menu w-100">
+                  <li>
+                    <a
+                      className="dropdown-item categories-dropdown-item"
+                      href="#"
+                    >
+                      Low to High
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item categories-dropdown-item"
+                      href="#"
+                    >
+                      High to Low
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item categories-dropdown-item"
+                      href="#"
+                    >
+                      Recently Listed
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-2 col-lg-3">
+              <div className="categories-dropdown p-2 d-flex align-items-center justify-content-center gap-2">
+                <img src={confluxIcon} alt="" />
+                <h6 className="token-title mb-0">WCFX</h6>
+              </div>
             </div>
           </div>
+
           {offerData.length === 0 ? (
-            <button
-              className={`btn ${
-                status === "fail" || approvestatus === "fail"
-                  ? "errorbtn"
-                  : "mint-now-btn"
-              } gap-2 align-self-center align-self-xxl-end align-self-xl-end align-self-lg-end mt-4`}
-              style={{ width: "fit-content" }}
-            //   onClick={() => {
-            //     isApprove
-            //       ? handleMakeOffer(price, filter1 === "weth" ? 0 : 1, filter1)
-            //       : approveMakeOffer(
-            //           price,
-            //           filter1 === "weth" ? 0 : 1,
-            //           filter1
-            //         );
-            //   }}
-            >
-              {status !== "fail " ||
-                (!isApprove && <img src={whiteTag} alt="" />)}
-              {status === "initial" ? (
-                isApprove ? (
-                  "Make offer"
-                ) : approvestatus === "initial" ? (
-                  "Approve"
-                ) : approvestatus === "loading" ? (
-                  <>
-                    Approving{" "}
-                    <div
-                      className="spinner-border mx-1"
-                      role="status"
-                      style={{ width: 16, height: 16 }}
-                    ></div>
-                  </>
-                ) : (
-                  "Failed"
-                )
-              ) : status === "loading" ? (
-                <>
-                  Making offer{" "}
-                  <div
-                    className="spinner-border mx-1"
-                    role="status"
-                    style={{ width: 16, height: 16 }}
-                  ></div>
-                </>
-              ) : status === "success" ? (
-                "Success"
-              ) : (
-                "Failed"
-              )}
-            </button>
+            // <button
+            //   className={`btn ${
+            //     status === "fail" || approvestatus === "fail"
+            //       ? "errorbtn"
+            //       : "makeoffer-btn"
+            //   } gap-2 align-self-center align-self-xxl-end align-self-xl-end align-self-lg-end mt-4`}
+            //   style={{ width: "fit-content" }}
+
+            // >
+            //   {status !== "fail " ||
+            //     (!isApprove && <img src={whiteTag} alt="" />)}
+            //   {status === "initial" ? (
+            //     isApprove ? (
+            //       "Make Offer"
+            //     ) : approvestatus === "initial" ? (
+            //       "Approve"
+            //     ) : approvestatus === "loading" ? (
+            //       <>
+            //         Approving{" "}
+            //         <div
+            //           className="spinner-border mx-1"
+            //           role="status"
+            //           style={{ width: 16, height: 16 }}
+            //         ></div>
+            //       </>
+            //     ) : (
+            //       "Failed"
+            //     )
+            //   ) : status === "loading" ? (
+            //     <>
+            //       Making offer{" "}
+            //       <div
+            //         className="spinner-border mx-1"
+            //         role="status"
+            //         style={{ width: 16, height: 16 }}
+            //       ></div>
+            //     </>
+            //   ) : status === "success" ? (
+            //     "Success"
+            //   ) : (
+            //     "Failed"
+            //   )}
+            // </button>
+            <div className="d-flex align-items-center justify-content-center w-100">
+              <button className="makeoffer-btn">Make Offer</button>
+            </div>
           ) : (
-            <div className="d-flex align-items-center gap-2 justify-content-between w-100">
-              <button
+            <div className="d-flex align-items-center gap-2 justify-content-center w-100">
+              {/* <button
                 className={`btn ${
                   deletestatus === "faildelete" ? "errorbtn" : "mint-now-btn"
                 } gap-2  align-self-center align-self-xxl-end align-self-xl-end align-self-lg-end  mt-4`}
@@ -307,7 +320,6 @@ const MakeOffer = ({
                   "Failed"
                 )}
               </button>
-
               <button
                 className={`btn ${
                   updatestatus === "failupdate" ? "errorbtn" : "pill-btn"
@@ -338,12 +350,15 @@ const MakeOffer = ({
                 ) : (
                   "Failed"
                 )}
-              </button>
+              </button> */}
+              <button className="updateoffer-btn">Update</button>
+              <button className="deleteoffer-btn">Delete</button>
             </div>
           )}
         </div>
       </Box>
     </Modal>
+    </OutsideClickHandler>
   );
 };
 

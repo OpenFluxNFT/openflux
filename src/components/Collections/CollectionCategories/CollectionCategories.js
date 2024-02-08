83419742c468ae4c787ba6ff84af952d95ad4bab
AldiAlinj <aldialinj0@gmail.com>
2024-02-08 12:18:47 +0100

    Added filtering for collections

--- src/components/Collections/CollectionCategories/CollectionCategories.js
+++ src/components/Collections/CollectionCategories/CollectionCategories.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./_collectioncategories.scss";
 import Slider from "react-slick";
 import CollectionCard from "../../CollectionCard/CollectionCard";
@@ -8,11 +8,14 @@ import collectionCardPlaceholder3 from "./assets/collectionCardPlaceholder3.png"
 import collectionCardPlaceholder4 from "./assets/collectionCardPlaceholder4.png";
 import useWindowSize from "../../../hooks/useWindowSize";
 import { NavLink } from "react-router-dom";
+import { Skeleton } from "@mui/material";
 
-const CollectionCategories = ({allCollections}) => {
+const CollectionCategories = ({ allCollections }) => {
   const [category, setCategory] = useState("all");
+  const [collections, setCollections] = useState([]);
   const windowSize = useWindowSize();
   const sliderRef = useRef();
+  const [loading, setLoading] = useState(false);
 
   const settings = {
     dots: false,
@@ -26,6 +29,40 @@ const CollectionCategories = ({allCollections}) => {
     // dotsClass: "button__bar",
   };
 
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const changeCategory = (val) => {
+    let filteredCollections = [];
+    setLoading(true);
+    setCategory(val);
+    if (val === "all") {
+      setCollections(allCollections);
+    } else if (val === "virtual") {
+      filteredCollections = allCollections.filter((item) => {
+        return item.tags.includes("Virtual World");
+      });
+      setCollections(filteredCollections);
+    } else {
+      filteredCollections = allCollections.filter((item) => {
+        return item.tags.includes(capitalizeFirstLetter(val));
+      });
+      setCollections(filteredCollections);
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  };
+
+  const filteredCollections = allCollections.filter((item) => {
+    return item.tags.includes(capitalizeFirstLetter("gaming"));
+  });
+
+  useEffect(() => {
+    setCollections(allCollections);
+  }, [allCollections]);
+
   return (
     <div className="container-lg py-5">
       <div className="row">
@@ -37,7 +74,7 @@ const CollectionCategories = ({allCollections}) => {
             className={`trending-tab ${
               category === "all" && "trending-tab-active"
             } p-2 d-flex align-items-center gap-2`}
-            onClick={() => setCategory("all")}
+            onClick={() => changeCategory("all")}
           >
             <h6 className="mb-0">All</h6>
           </div>
@@ -45,7 +82,7 @@ const CollectionCategories = ({allCollections}) => {
             className={`trending-tab ${
               category === "gaming" && "trending-tab-active"
             } p-2 d-flex align-items-center gap-2`}
-            onClick={() => setCategory("gaming")}
+            onClick={() => changeCategory("gaming")}
           >
             <h6 className="mb-0">Gaming</h6>
           </div>
@@ -53,15 +90,15 @@ const CollectionCategories = ({allCollections}) => {
             className={`trending-tab ${
               category === "art" && "trending-tab-active"
             } p-2 d-flex align-items-center gap-2`}
-            onClick={() => setCategory("art")}
+            onClick={() => changeCategory("art")}
           >
             <h6 className="mb-0">Art</h6>
           </div>
           <div
             className={`trending-tab ${
-              category === "virtualWorld" && "trending-tab-active"
+              category === "virtual" && "trending-tab-active"
             } p-2 d-flex align-items-center gap-2`}
-            onClick={() => setCategory("virtualWorld")}
+            onClick={() => changeCategory("virtual")}
           >
             <h6 className="mb-0">Virtual World</h6>
           </div>
@@ -69,7 +106,7 @@ const CollectionCategories = ({allCollections}) => {
             className={`trending-tab ${
               category === "music" && "trending-tab-active"
             } p-2 d-flex align-items-center gap-2`}
-            onClick={() => setCategory("music")}
+            onClick={() => changeCategory("music")}
           >
             <h6 className="mb-0">Music</h6>
           </div>
@@ -77,7 +114,7 @@ const CollectionCategories = ({allCollections}) => {
             className={`trending-tab ${
               category === "sports" && "trending-tab-active"
             } p-2 d-flex align-items-center gap-2`}
-            onClick={() => setCategory("sports")}
+            onClick={() => changeCategory("sports")}
           >
             <h6 className="mb-0">Sports</h6>
           </div>
@@ -88,29 +125,68 @@ const CollectionCategories = ({allCollections}) => {
           ))}
         </Slider> */}
         {windowSize.width > 786 ? (
-          <div className="collection-categories-grid">
-            {allCollections.slice(0, 4).map((item, index) => (
-              <NavLink
-                to={`/collection/${item.contractAddress}/${item.symbol}`}
-                key={index}
-                className={"text-decoration-none"}
+          <>
+            {loading === true ? (
+              <div className="collection-categories-grid">
+                <Skeleton variant="rounded" width={310} height={353} />
+                <Skeleton variant="rounded" width={310} height={353} />
+                <Skeleton variant="rounded" width={310} height={353} />
+                <Skeleton variant="rounded" width={310} height={353} />
+              </div>
+            ) : loading === false && collections.length > 0 ? (
+              <div className="collection-categories-grid">
+                {collections
+                  .slice(collections.length - 4, collections.length)
+                  .map((item, index) => (
+                    <NavLink
+                      to={`/collection/${item.contractAddress}/${item.symbol}`}
+                      key={index}
+                      className={"text-decoration-none"}
+                    >
+                      <CollectionCard key={index} data={item} />
+                    </NavLink>
+                  ))}
+              </div>
+            ) : (
+              <div
+                className="d-flex align-items-center justify-content-center w-100"
+                style={{ height: 353 }}
               >
-                <CollectionCard key={index} data={item} />
-              </NavLink>
-            ))}
-          </div>
+                <h6 className="text-white">
+                  There are no collections available for this category
+                </h6>
+              </div>
+            )}
+          </>
         ) : (
-          <Slider ref={sliderRef} {...settings}>
-            {allCollections.map((item, index) => (
-              <NavLink
-              to={`/collection/${item.contractAddress}/${item.symbol}`}
-                className={"text-decoration-none"}
-                key={index}
+          <>
+            {loading === true ? (
+              <Slider ref={sliderRef} {...settings}>
+                <Skeleton variant="rounded" width={"100%"} height={351} />
+              </Slider>
+            ) : loading === false && collections.length > 0 ? (
+              <Slider ref={sliderRef} {...settings}>
+                {collections.map((item, index) => (
+                  <NavLink
+                    to={`/collection/${item.contractAddress}/${item.symbol}`}
+                    className={"text-decoration-none"}
+                    key={index}
+                  >
+                    <CollectionCard key={index} data={item} />
+                  </NavLink>
+                ))}
+              </Slider>
+            ) : (
+              <div
+                className="d-flex align-items-center justify-content-center w-100"
+                style={{ height: 353 }}
               >
-                <CollectionCard key={index} data={item} />
-              </NavLink>
-            ))}
-          </Slider>
+                <h6 className="text-white" style={{textAlign: "center"}}>
+                  There are no collections available for this category
+                </h6>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>

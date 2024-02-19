fad6a4be4fcc29e3419ff5623208eedd35ced3b7
LorenaL579 <lorena.lici579@gmail.com>
2024-02-19 10:02:58 +0100

    fixes for floorprice and volume

--- src/components/Home/HomeStats/HomeStats.js
+++ src/components/Home/HomeStats/HomeStats.js
@@ -28,8 +28,6 @@ const HomeStats = ({ cfxPrice }) => {
       "https://confluxapi.worldofdypians.com/api/total-volume"
     );
     setTotalVolume((response.data.totalVolume / 1e18) * cfxPrice);
-
-    console.log(response.data.totalVolume, cfxPrice);
   };
 
   useEffect(() => {
@@ -38,13 +36,13 @@ const HomeStats = ({ cfxPrice }) => {
     fetchTotalTxs();
   }, [cfxPrice]);
 
-  console.log(totalVolume, "total");
-
   return (
     <div className="container-fluid  py-5 homestats-wrapper">
       <div className="container-lg px-0 d-flex flex-column flex-lg-row align-items-center justify-content-between mt-4 gap-2 gap-lg-0">
         <div className="hero-stats-item p-5 d-flex flex-column align-items-center justify-content-center gap-2">
-          <h6 className="stats-item-amount mb-0">{getFormattedNumber(totalTxs, 0)}</h6>
+          <h6 className="stats-item-amount mb-0">
+            {getFormattedNumber(totalTxs, 0)}
+          </h6>
           <span className="stats-item-desc mb-0">Total transactions</span>
         </div>
         <div className="hero-stats-item p-5 d-flex flex-column align-items-center justify-content-center gap-2">
